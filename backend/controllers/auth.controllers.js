92ab72adfbfc39770e80d9d4897b6a3788634072
gauravdevkar27 <devkardinesh18@gmail.com>
2025-08-18 00:49:13 +0530

    Initial commit of MERN To-Do application

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,72 +2,58 @@ const User = require('../models/user.models.js');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+// Custom async handler to replace express-async-handler
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
 const secretKey = process.env.JWT_SECRET;
 
-async function registerUser(req, res){
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, userName, passWord } = req.body;
 
+  if (!userName || !passWord) {
+    res.status(400);
+    throw new Error('Please provide a username and password');
+  }
 
-    let {firstName, lastName, userName, passWord} = req.body;
+  const duplicate = await User.findOne({ userName });
+  if (duplicate) {
+    res.status(400);
+    throw new Error('User already registered with this username');
+  }
 
-    try{
-      const duplicate = await User.find({userName});
-      if(duplicate && duplicate.length > 0) 
-      {
-        return res
-        .status(400)
-        .send(
-            {message: 'User already registerd with this username'}
-        );
-    }
-    let user = new User({firstName, lastName, userName, passWord});
-    const result = await user.save();
-    console.log(result);
+  const user = new User({ firstName, lastName, userName, passWord });
+  const result = await user.save();
 
-    res
-    .status(201)
-    .send(
-        {message: 'User registered successfully!'}
-    );
-    } catch(err){
-        console.log(err);
-        res
-        .status(400)
-        .send(err);
-    }
-}
+  if (result) {
+    res.status(201).json({ message: 'User registered successfully!' });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
-async function loginUser(req, res){
-    try{
-        const {userName, passWord} = req.body;
-        const user = await User.findOne({userName});
-        if(!user){
-            return res
-                   .status(404)
-                   .send({error: "Authentication failed"});
-        }
-        const isPasswordValid = await user.comparePassword(passWord);   //if password is match then it is valid
-        if(!isPasswordValid){
-            return res
-                   .status(404)
-                   .send({message: "Wrong password"});
-        }
-        let token =  jwt.sign({userId:user?._id},secretKey, {expiresIn: '1h'});
-        let finalData = {
-            userId:user?._id,
-            userName:user?.userName,
-            firstName:user?.firstName,
-            lastName:user?.lastName,
+const loginUser = asyncHandler(async (req, res) => {
+  const { userName, passWord } = req.body;
+  const user = await User.findOne({ userName });
 
-            token
-        }
-        res.send(finalData);
-    } catch(err){
-        console.log(err);
-        res
-        .status(400)
-        .send(err);
-    }
-}
+  if (user && (await user.comparePassword(passWord))) {
+    const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
+    const finalData = {
+      userId: user._id,
+      userName: user.userName,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token: token,
+    };
+
+    res.status(200).json(finalData);
+  } else {
+    res.status(401); // Use 401 Unauthorized for login failures
+    throw new Error('Invalid username or password');
+  }
+});
 
 const AuthController = {
     registerUser,
--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -5,13 +5,13 @@ const bcrypt = require('bcrypt');
 const userSchema = new Schema({
     firstName: String,
     lastName: String,
-    userName: {type:String, required:true},
+    userName: {type:String, required:true, unique:true},
     passWord: {type:String, required:true}
 });
 
 userSchema.pre("save", async function (next) {
     const user = this;
-    if(!user.isModified('password')) return next(); // if user not modified password then goto next function
+    if(!user.isModified('passWord')) return next(); // if user not modified password then goto next function
 
     let salt = await bcrypt.genSalt(10);
     let hash = await bcrypt.hash(user.passWord, salt);
@@ -20,7 +20,7 @@ userSchema.pre("save", async function (next) {
 })
 
 userSchema.methods.comparePassword = async function (passWord) {     //wheather user enter password is matching our database or not
-    return bcrypt.compare(passWord, this.passWord);
+    return await bcrypt.compare(passWord, this.passWord);
 }
 
 
